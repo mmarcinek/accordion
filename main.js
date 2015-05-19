11ca3f752925521bcddffccc816eb8a142459722
Michael Marcinek <michaelmarcinek@gmail.com>
2015-05-19 16:50:38 -0400

    Adds comments about how jQuery is working

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ $('section').on('click', function(){
   $('section').removeClass('home');
   $(this).addClass('home');
 });
+
+//Page opens with the first section loaded in full by default;
+//We target the <section> with $('section')
+// on the event 'click', section removes the Class element attributed in css as 'home'
+// and replaces it with the default css for the section as attributed in css or if section
+// is already loaded swaps for 'home' stling.
