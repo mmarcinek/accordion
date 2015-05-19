b5432c2623de957f30778e62ce2feb4b78a0d07f
Michael Marcinek <michaelmarcinek@gmail.com>
2015-05-19 16:27:37 -0400

    Fixes all the jQuery JavaScripts

---
+++ main.js
@@ -0,0 +1,9 @@
+// $('.p2').addClass('hide');
+// $('.p3').addClass('hide');
+
+// $('.p1').on('click', function())
+
+$('section').on('click', function(){
+  $('section').removeClass('home');
+  $(this).addClass('home');
+});
